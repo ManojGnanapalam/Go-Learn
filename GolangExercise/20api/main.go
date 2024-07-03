ea20daaf77d3bf904613ba7e8dad79083a22c231
ManojGnanapalam <manojgsret@gmail.com>
2024-07-03 21:44:33 +0530

    completed till API

--- GolangExercise/20api/main.go
+++ GolangExercise/20api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -25,12 +26,27 @@ type Address struct {
 
 var userData []Users
 
-func (c *Users) IsEmpty() bool {
-	return c.Name == ""
+func (u *Users) IsEmpty() bool {
+	return u.Name == ""
 }
 
-func main() {
+// func (u *Users) IsIdEqueal(Id string) bool{
 
+// }
+
+func main() {
+	r := mux.NewRouter()
+
+	userData = append(userData, Users{UserId: "2", Name: "viper", Email: "viper@goo.co", Role: []string{"user", "editer"}, Address: &Address{DoorNo: 14, City: "chennai"}})
+
+	r.HandleFunc("/", MainPage).Methods("GET")
+	r.HandleFunc("/users", getAllUsers).Methods("GET")
+	r.HandleFunc("/user/{id}", getOneUser).Methods("GET")
+	r.HandleFunc("/user", createNewUser).Methods("POST")
+	r.HandleFunc("/user/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/delete/{id}", deleteOneUser).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
@@ -50,11 +66,9 @@ func getOneUser(w http.ResponseWriter, r *http.Request) {
 	for _, user := range userData {
 		if user.UserId == params["id"] {
 			json.NewEncoder(w).Encode(user)
-			return
 		}
 	}
 	json.NewEncoder(w).Encode("No user found with given id")
-	return
 
 }
 
@@ -78,17 +92,38 @@ func createNewUser(w http.ResponseWriter, r *http.Request) {
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
+	isIdFound := false
 	params := mux.Vars(r)
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Request has no data")
+	}
 
 	for i, user := range userData {
 		if user.UserId == params["id"] {
+			isIdFound = true
 			userData = append(userData[:i], userData[i+1:]...)
+
 			var user Users
 			_ = json.NewDecoder(r.Body).Decode(&user)
 			user.UserId = params["id"]
 			userData = append(userData, user)
 			json.NewEncoder(w).Encode(user)
+		}
+	}
+
+	if !isIdFound {
+		json.NewEncoder(w).Encode("Id is not founded")
+	}
+}
+
+func deleteOneUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, user := range userData {
+		if user.UserId == params["id"] {
+			userData = append(userData[:i], userData[i+1:]...)
+			break
 		}
 	}
 }
